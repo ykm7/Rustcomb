520740db5f0d0648133b32d2dfe8055bc0d1c2e5
Yoakim <ykmpersson@gmail.com>
2025-03-09 18:10:21 +0800

    1. Added benchmark steps for the rest

--- README.md
+++ README.md
@@ -4,5 +4,14 @@
 
 https://crates.io/crates/wild
 
-General testing setup.
+General 
 > cargo run .txt .\test_file_direction\ test
+
+## Benchmarking
+As part of my continued understanding of how Rust operations I have established benchmarks of all file retrieving and parsing
+
+> cargo bench
+
+## Linting (Clippy)
+
+Clippy is [used](https://github.com/rust-lang/rust-clippy) to try to pick up addition issues/suggestions. Very handy while learning.
--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,14 +1,17 @@
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
 
-use rustcomb::{Cli, single_thread_read_files,rayon_read_files};
+use rustcomb::{
+    Cli, rayon_read_files, single_thread_read_files, thread_per_file_read_files,
+    threadpool_read_files,
+};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let cli = Cli {
         path_pattern: ".txt".to_string(),
         path: ".\\test_file_direction\\".into(),
-        file_pattern: "test".to_string()
+        file_pattern: "test".to_string(),
     };
-    
+
     c.bench_function(
         format!("'single_thread_read_files': {:?}\n", cli).as_str(),
         |b| {
@@ -16,7 +19,7 @@ fn criterion_benchmark(c: &mut Criterion) {
                 let cli = Cli {
                     path_pattern: ".txt".to_string(),
                     path: ".\\test_file_direction\\".into(),
-                    file_pattern: "test".to_string()
+                    file_pattern: "test".to_string(),
                 };
                 single_thread_read_files(black_box(cli))
             })
@@ -24,18 +27,57 @@ fn criterion_benchmark(c: &mut Criterion) {
     );
 
     c.bench_function(
-        format!("'rayon_read_files': {:?}\n", cli).as_str(),
+        format!("'thread_per_file_read_files': {:?}\n", cli).as_str(),
         |b| {
             b.iter(|| {
                 let cli = Cli {
                     path_pattern: ".txt".to_string(),
                     path: ".\\test_file_direction\\".into(),
-                    file_pattern: "test".to_string()
+                    file_pattern: "test".to_string(),
                 };
-                rayon_read_files(black_box(cli))
+                thread_per_file_read_files(black_box(cli))
             })
         },
     );
+
+    c.bench_function(
+        format!("'use_thread_pool - 1 thread': {:?}\n", cli).as_str(),
+        |b| {
+            b.iter(|| {
+                let cli = Cli {
+                    path_pattern: ".txt".to_string(),
+                    path: ".\\test_file_direction\\".into(),
+                    file_pattern: "test".to_string(),
+                };
+                threadpool_read_files(black_box(cli), 1)
+            })
+        },
+    );
+
+    c.bench_function(
+        format!("'use_thread_pool - {}': {:?}\n", num_cpus::get(), cli).as_str(),
+        |b| {
+            b.iter(|| {
+                let cli = Cli {
+                    path_pattern: ".txt".to_string(),
+                    path: ".\\test_file_direction\\".into(),
+                    file_pattern: "test".to_string(),
+                };
+                threadpool_read_files(black_box(cli), num_cpus::get())
+            })
+        },
+    );
+
+    c.bench_function(format!("'rayon_read_files': {:?}\n", cli).as_str(), |b| {
+        b.iter(|| {
+            let cli = Cli {
+                path_pattern: ".txt".to_string(),
+                path: ".\\test_file_direction\\".into(),
+                file_pattern: "test".to_string(),
+            };
+            rayon_read_files(black_box(cli))
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
